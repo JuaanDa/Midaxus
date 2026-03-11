8c3ccf5ea5bb0da1ca7212a1aac0f37cf0dea5e4
Danieel07 <dnietollanos@gmail.com>
2026-03-10 21:44:36 -0500

    First Commit

---
+++ src/main/java/com/example/Midaxus/MidaxusApplication.java
@@ -0,0 +1,13 @@
+package com.example.Midaxus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidaxusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidaxusApplication.class, args);
+	}
+
+}
